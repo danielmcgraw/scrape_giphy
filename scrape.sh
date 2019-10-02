89364e1081a4eba6ba1fb5eb25ac2df2ba5352be
Daniel McGraw <dan.j.mcgraw@gmail.com>
2019-10-01 20:34:19 -0600

    Make delay an argument

--- scrape.sh
+++ scrape.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-if [ "$#" -ne 3 ]
+if [ "$#" -ne 4 ]
 then
 	echo "Missing arguments"
-	echo "./scrape.sh <keyword> <save_path> <api_key>"
+	echo "./scrape.sh <keyword> <save_path> <api_key> <call_delay>"
 	exit 1
 fi
 
 while true;
 do
 	API_KEY=$3 node scrape_giphy.js $1 $2;
-	sleep 300;
+	sleep $4;
 done
