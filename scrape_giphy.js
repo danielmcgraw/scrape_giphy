2211e4aebfbcc0ae833c0073cf4b8e5efb603ceb
Daniel McGraw <dan.j.mcgraw@gmail.com>
2019-10-06 08:57:32 -0600

    Protect against empty responses

--- scrape_giphy.js
+++ scrape_giphy.js
@@ -8,22 +8,26 @@ const req = https.get(`https://api.giphy.com/v1/gifs/random?tag=${keyword}&api_k
 	res.on('data', (chunk) => { body += chunk; });
 	res.on('end', () => {
     body = JSON.parse(body);
-		const filename = body.data.image_mp4_url.split('/')[4];
-		const file_path = `${root}/${keyword}/${filename}.mp4`
-		const req = https.get(body.data.image_mp4_url, (resp) => {
-			fs.mkdir(`${root}/${keyword}`, (err) => {
-				if (err && err.code != 'EEXIST') throw 'up'
-				fs.access(file_path, fs.F_OK, (e) => {
-					if (e) {
-						console.log(`Getting: ${body.data.image_mp4_url}`);
-						const file = fs.createWriteStream(file_path);
-						resp.pipe(file);
-					} else {
-						console.log(`Skipping: ${body.data.image_mp4_url}, already exists`);
-					}
+		if (body.meta.response_id) {
+			const filename = body.data.image_mp4_url.split('/')[4];
+			const file_path = `${root}/${keyword}/${filename}.mp4`
+			const req = https.get(body.data.image_mp4_url, (resp) => {
+				fs.mkdir(`${root}/${keyword}`, (err) => {
+					if (err && err.code != 'EEXIST') throw 'up'
+					fs.access(file_path, fs.F_OK, (e) => {
+						if (e) {
+							console.log(`Getting: ${body.data.image_mp4_url}`);
+							const file = fs.createWriteStream(file_path);
+							resp.pipe(file);
+						} else {
+							console.log(`Skipping: ${body.data.image_mp4_url}, already exists`);
+						}
+					});
 				});
 			});
-		});
+		} else {
+			console.log('Empty response');
+		}
   });
 }).on('error', (e) => {
   console.log("Got error: " + e.message);
